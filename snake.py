3ed197c68d4793b45c14e4c5498168449a116a20
Yakov <167911620+GitW1n@users.noreply.github.com>
2024-12-03 22:43:26 +0300

    Update snake.py

--- snake.py
+++ snake.py
@@ -67,6 +67,7 @@ class snake():
                     self.dirnx = -1
                     self.dirny = 0
                     self.turns[self.head.pos[:]] = [self.dirnx,self.dirny]
+                
                 elif keys[pygame.K_RIGHT]:
                     self.dirnx = 1
                     self.dirny = 0
