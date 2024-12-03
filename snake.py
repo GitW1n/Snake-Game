047cbaad68f8eb39b9895f2063c83733bb9662c7
Yakov <167911620+GitW1n@users.noreply.github.com>
2024-12-03 22:43:15 +0300

    Update snake.py

--- snake.py
+++ snake.py
@@ -79,6 +79,7 @@ class snake():
                     self.dirny = 1
                     self.dirnx = 0
                     self.turns[self.head.pos[:]] = [self.dirnx,self.dirny]
+                
                 if self.head.pos[0] < 0 or self.head.pos[0] >= rows or self.head.pos[1] < 0 or self.head.pos[1] >= rows:
             self.reset((10, 10))
             
